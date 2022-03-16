74643f8ba1983783242f13cbd5cec980f6f683d2
Armani Brown <61952310+ChromeMatic@users.noreply.github.com>
2022-03-16 16:29:21 -0500

    additional changes added

--- src/store/modules/crypto.js
+++ src/store/modules/crypto.js
@@ -1,16 +1,25 @@
 import axios from "axios";
 
 export const state = {
-    crypto: {}
+    crypto: {},
+    cryptoID: {}
 };
 
 export const getters = {
     getCrypto(state) {
-        return state.crypto
+        return state.crypto;
+    },
+    getCryptoByID(state) {
+        return state.cryptoID;
+    },
+    getCryptoChartInfo(state) {
+
     }
 };
 
 export const actions = {
+
+    //Get cryptocurrency information
     async FetchCrypto({ commit }) {
         await axios.get(
             import.meta.env.VITE_URL,
@@ -21,11 +30,24 @@ export const actions = {
         ).catch(error => {
             alert('Error has happen!');
         });
+    },
+
+    //Get cryptocurrency information by id
+    async GetCryptoByID({ commit }) {
+
+    },
+
+    //Get cryptocurrency chart data by ID
+    async getChartData({ commit }, { id }) {
+        let ID = id + '/market_chart?vs_currency=usd&days=1&interval=10';
     }
 };
 
 export const mutations = {
     SetCrypto(state, payload) {
         state.crypto = payload;
+    },
+    SetCryptoID(state, payload) {
+        state.cryptoID = payload;
     }
 };
