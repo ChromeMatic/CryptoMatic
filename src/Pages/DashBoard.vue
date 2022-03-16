cd9227918755fc5a7e52e4b2da3cdfcb37fb4a6a
Armani Brown <61952310+ChromeMatic@users.noreply.github.com>
2022-03-15 23:21:31 -0500

    additional css changes added to dashboard.vue

--- src/Pages/DashBoard.vue
+++ src/Pages/DashBoard.vue
@@ -25,8 +25,8 @@ export default {
         
         <SearchBar/>
 
-        <div class="rounded w-full grid gap-2 lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-3 xs:grid-cols-2 justify-center items-center p-2 font-semibold text-gray-700 dark:text-white">
-           <div v-for="crypto in getCrypto" :key="crypto['id']" class="flex flex-col space-y-2 justify-center items-center dark:bg-gray-800 rounded-md"> 
+        <div class="rounded w-full grid gap-2 lg:grid-cols-5 md:grid-cols-3 sm:grid-cols-3 xs:grid-cols-2 justify-center items-center p-2 font-semibold text-gray-500 dark:text-white">
+           <div v-for="crypto in getCrypto" :key="crypto['id']" class="flex flex-col space-y-2 justify-center items-center bg-pr1 dark:bg-gray-900 rounded-md"> 
              {{crypto['name']}}
              <img :src="crypto['image']" class="h-8 w-8" alt="">
              <p>${{crypto['current_price']}}</p>
