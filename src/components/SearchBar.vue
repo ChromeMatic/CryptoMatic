2fab9d6638133ced65ada3b45a06ff7a1a3089be
Armani Brown <61952310+ChromeMatic@users.noreply.github.com>
2022-03-16 16:47:32 -0500

    additional css changes

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -8,7 +8,7 @@ export default {
    computed:mapGetters(["getTheme"]),
    methods:{
        findCoin(){
-
+           let coinName =  this.searchWord;
        }
    }
 } 
@@ -16,9 +16,9 @@ export default {
 
 <template>
  <div :class="getTheme ? 'dark' : ''" >
-    <div class="bg-gray-50 dark:bg-x flex lg:py-6 lg:px-4">
+    <div class="bg-pr1 dark:bg-x flex lg:py-2 lg:px-8 rounded-md">
 
-        <div class="rounded py-2 px-2 flex flex-1 justify-center items-center space-x-4">
+        <div class="py-2 px-2 flex flex-1 justify-center items-center space-x-4">
 
            <div class="flex space-x-1 justify-center items-center">
                 <h1 class="text-gray-600 dark:text-white uppercase text-2xl font-semibold">
@@ -27,7 +27,7 @@ export default {
            </div>
 
            <div>
-              <input type="text" v-model="searchWord" class="bg-gray-50 border text-gray-900 text-xl font-s rounded-lg focus:ring-blue-500 focus:border-blue-500 block lg:w-96 w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Bitcoin" required>
+              <input type="text" v-model="searchWord" class="bg-white border text-gray-900 text-xl font-s rounded-lg focus:ring-blue-500 focus:border-blue-500 block lg:w-96 w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500" placeholder="Bitcoin" required>
            </div>
 
            <button @click="findCoin" class="py-1 px-2 rounded bg-green-500 dark:bg-gray-600">
